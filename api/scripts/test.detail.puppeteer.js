114e166aa84a66b156245bf88fa8e9a31b5759c7
¨LuisEUM¨ <luiseum95@gmail.com>
2022-11-29 18:40:55 +0100

    List of followers obtained...

--- api/scripts/test.detail.puppeteer.js
+++ api/scripts/test.detail.puppeteer.js
@@ -11,7 +11,7 @@ module.exports.test = async (username) => {
         'Accept-Language': 'en'
     });
     
-    console.log('Loading Login Page')
+    console.info('Loading Login Page')
 
     await page.goto('https://www.instagram.com/accounts/login', { 
         waitUntil: "networkidle2",
@@ -26,9 +26,9 @@ module.exports.test = async (username) => {
 
     if (notifyBtn.length> 0) {
         await notifyBtn[0].click();
-        console.log('Notification button clicked.')
+        console.info('Notification button clicked.')
     } else {
-        console.log("No notification button to click.");
+        console.info("No notification button to click.");
     }
 
     await page.type('input[name=username]', process.env.USERNAME_INSTAGRAM, { delay: 60 });
@@ -37,16 +37,16 @@ module.exports.test = async (username) => {
     await Promise.all([
         page.waitForNavigation(), // The promise resolves after navigation has finished
         await page.click('button[type=submit]', { delay: 20 }),
-        console.log('waiting for submit')
+        console.info('waiting for submit')
     ]);
 
-    console.log('You are logged now...')
+    console.info('You are logged now...')
 
 
     await Promise.all([
         page.waitForNavigation(), // The promise resolves after navigation has finished
         await page.goto(`https://www.instagram.com/${username}`, { waitUntil: "networkidle2" }),
-        console.log(`searching username ${username}`)
+        console.info(`searching username ${username}`)
     ]);
 
     //THIS ARE DIFERENTS WAYS TO GET THE ELEMENTHANDLE 
@@ -58,17 +58,17 @@ module.exports.test = async (username) => {
     const followers = await page.$eval(`a[href="/${username}/followers/"] > div > span`, element => element.textContent)
     const following = await page.$eval(`a[href="/${username}/following/"] > div > span`, element => element.textContent) 
 
-    console.log(`Followers of ${username}: ${followers}`, ` ${username} is following to: ${following}`)
+    console.info(`Followers of ${username}: ${followers}`, ` ${username} is following to: ${following}`)
 
-    console.log('Option 1 getting usernames clicking the followers button')
+    console.info('Option 1 getting usernames clicking the followers button')
     const followersBtn = await page.$x("//div[contains(text(), ' followers')]/span");
 
     if (followersBtn.length > 0) {
         await followersBtn[0].evaluate(btn => btn.click());
-        console.log( 'Followers button clicked')
+        console.info( 'Followers button clicked')
     } 
 
-    console.log( 'Wating for the popup')
+    console.info( 'Wating for the popup')
     const popup ='div._aano'
     const scroller ='div._aano > div:nth-child(1)'
 
@@ -78,7 +78,7 @@ module.exports.test = async (username) => {
     await page.waitForSelector(usernamesHref);
 
     const totalFollowersNumber = parseInt(followers.split(',').join('')) + 0
-    console.log('Followers string transformed into Number', totalFollowersNumber)
+    console.info('Followers string transformed into Number', totalFollowersNumber)
 
     await scrapeInfiniteScrollItems(page, totalFollowersNumber, popup, usernamesHref, scroller);
     // const items = await scrapeInfiniteScrollItems(page, totalFollowersNumber, followersDialog, usernamesHref);
@@ -86,107 +86,60 @@ module.exports.test = async (username) => {
 
 
 const scrapeInfiniteScrollItems = async (page, followers, selector, listSelector, scroller) => {
-    console.log('Testing.... Wheel moved')
-
-
-
     let followersCounter = 0;
     let items  = [];
-
+    let checkingTheEnd = []
 
 
     while (followers >= followersCounter) {
-        let box = await page.$(selector);
-        let boundingBox = await box.boundingBox();
-        let previousHeight = boundingBox.height;
-
         let scrollerBox = await page.$(scroller);
         let boundingScrollerBox = await scrollerBox.boundingBox();
         let previousboundingScrollerBox = boundingScrollerBox.height;
 
-        await page.evaluate((previousHeight, selector, previousboundingScrollerBox, scroller) => {
+
+        let lastPreviousboundingScrollerBox = await page.evaluate(( selector, previousboundingScrollerBox) => {
             let selectioned = document.querySelector(selector)
+                selectioned.scrollTo(0, previousboundingScrollerBox);
+                return previousboundingScrollerBox 
+        },  selector, previousboundingScrollerBox);     
 
-            if(previousHeight <= previousboundingScrollerBox) {
-                console.log('entro')
-                selectioned.scrollTo(0, previousHeight);
-                return
-            }
-        }, previousHeight, selector, previousboundingScrollerBox, scroller);    
-
-        // await page.evaluate((previousHeight, selector, previousboundingScrollerBox) => {
-        //     let selectioned = document.querySelector(selector)
-
-        //     if(previousHeight <= previousboundingScrollerBox) {
-        //         console.log('entro')
-        //          selectioned.scrollTo(0, previousHeight);
-        //          return
-        //     }
-        // }, previousHeight, selector, previousboundingScrollerBox);    
-
-        console.log(followers, followersCounter, previousHeight, previousboundingScrollerBox)
+        console.info('Loading more users...')
+        
+        followersCounter++
 
         await page.waitForTimeout(5000);
-        followersCounter++
-        // await page.waitForFunction(boundingBox.height > previousHeight);
-        // await new Promise((resolve) => setTimeout(resolve, 10000));
+
+        lastPreviousboundingScrollerBox = await page.evaluate((selector) => {
+            let selectioned = document.querySelector(selector)
+            return selectioned.scrollTop;
+        }, selector);  
+
+        if(followersCounter % 2 === 0){
+            if (checkingTheEnd[0] === lastPreviousboundingScrollerBox){
+                break
+            } else {
+                checkingTheEnd[0] = lastPreviousboundingScrollerBox
+            }
+        }
+
+        console.info("Number of Followers:", followers, "Loop Counter:", followersCounter, "Current:", previousboundingScrollerBox, "Current:", lastPreviousboundingScrollerBox)
     }
 
-    console.log('Testing... salio o no entro al bucle')
+    console.info("You managed to escape from the loop and now you will obtained the list of links with all the followers")
 
 
  //THIS IS THE ARRAY LIST WITH ALL THE ELEMENTS 
-    items = await page.evaluate(listSelector => {
+    const listOfFollowers = await page.evaluate(listSelector => {
         return [...document.querySelectorAll(listSelector)].map(anchor => {
-            const title = anchor.textContent.split('|')[0].trim();
-            return `${title} - ${anchor.href}`;
+            return `${anchor.href}`;
+            // const title = anchor.textContent.split('|')[0].trim();
+            // return `${title} - ${anchor.href}`;
         });
     }, listSelector);
 
-    console.log(items.join('\n'));
-    console.log('items', items, 'items Length', items.length, 'itemTargetCount', itemTargetCount)
+    console.info(listOfFollowers.join('\n'));
+    console.info('List of Followers:', listOfFollowers, 'Total of followers recoleted:', listOfFollowers.length)
+    console.info("Done with the request, thanks for the patience...")
 
-    return items;
+    return listOfFollowers;
 }
-
-
-
-
-
-
-
-// while (followers >= followers) {
-//     let previousHeight = boundingBox.height;
-
-//     items = await page.evaluate(listSelector => {
-//         return [...document.querySelectorAll(listSelector)].map(anchor => {
-//             const title = anchor.textContent.split('|')[0].trim();
-//             return `${title} - ${anchor.href}`;
-//         });
-//     }, listSelector);
-
-
-//     console.log('THIS IS HAPPENING')
-
-//     await page.evaluate((previousHeight, selector) => {
-//         let selectioned = document.querySelector(selector)
-//         selectioned.scrollTo(0, previousHeight);    
-//     }, previousHeight, selector);    
-
-//     followersCounter++
-//     console.log(followersCounter, previousHeight)
-//     await page.waitForFunction(boundingBox.height > previousHeight);
-//     await new Promise((resolve) => setTimeout(resolve, 5000));
-
-// }
-
-// console.log('Testing... salio o no entro al bucle')
-
-
-
-
-
-// console.log(items.join('\n'));
-// console.log('items', items, 'items Length', items.length, 'itemTargetCount', itemTargetCount)
-
-// return items;
