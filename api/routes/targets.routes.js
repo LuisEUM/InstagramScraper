8aab1c3fa65f7771ed94ae157ee047654adca8ba
¨LuisEUM¨ <luiseum95@gmail.com>
2022-12-06 21:01:49 +0100

    Improving documentation and dependencies

--- api/routes/targets.routes.js
+++ api/routes/targets.routes.js
@@ -5,7 +5,7 @@ const secure = require("../middlewares/secure.mid.js");
 
 router.get("/:id", secure.isAuthenticated, target.detail); 
 router.get("/", secure.isAuthenticated, target.list);
-router.post("/", secure.isAuthenticated,secure.isTargetOwnedByUser, target.create);
+router.post("/", secure.isAuthenticated, target.create);
 router.get("/:id/followers", secure.isAuthenticated, secure.isTargetOwnedByUser, target.followers); 
 router.patch("/:id", secure.isAuthenticated, secure.isTargetOwnedByUser, target.update);
 router.delete("/:id", secure.isAuthenticated, secure.isTargetOwnedByUser, target.delete);
